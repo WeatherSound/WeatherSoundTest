92d7bb8ee5317857084927279509e083124af669
JuliaHwang <qufskan9396@gmail.com>
2017-08-05 14:36:20 +0900

    add userlist/signup view, serializers, urls

--- django_app/member/serializers/__init__.py
+++ django_app/member/serializers/__init__.py
@@ -1,1 +1,3 @@
 from .auth import *
+from .signup import *
+from .user import *
--- django_app/member/serializers/user.py
+++ django_app/member/serializers/user.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import get_user_model
-from django.core.validators import validate_email
-from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 User = get_user_model()
+
+__all__ = (
+    'UserListSerializers',
+)
 
 
 class UserListSerializers(serializers.ModelSerializer):
