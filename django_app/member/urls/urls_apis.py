79e8e4d4f0860fb7ac0ac9dd125788083467e939
JuliaHwang <qufskan9396@gmail.com>
2017-08-05 13:40:25 +0900

    [auth] create login API - CustomAuthTokenSerializers to validate email and password, CustomAuthTokenView to show auth info and send token back

--- django_app/member/urls/urls_apis.py
+++ django_app/member/urls/urls_apis.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
-from member import views
+from member import views, apis
 
 urlpatterns = [
+    url(r'^login/',
+        apis.CustomAuthTokenView.as_view(),
+        name='login'
+        ),
+
     # url(r'^signup/$', views.signup, name='signup'),
     # url(r'^login/$', views.login, name='login'),
     # url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
