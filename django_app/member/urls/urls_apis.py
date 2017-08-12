831eaac084ef8c7ef692cd17e4df96be8561081c
JuliaHwang <qufskan9396@gmail.com>
2017-08-12 19:38:51 +0900

    [auth] refactor url - /member/pk/ == /member/profile/pk/

--- django_app/member/urls/urls_apis.py
+++ django_app/member/urls/urls_apis.py
@@ -10,6 +10,14 @@ urlpatterns = [
         apis.UserRetrieveUpdateDestroyView.as_view(),
         name='userinfo'
         ),
+    url(r'^profile/(?P<pk>\d+)/$',
+        apis.UserRetrieveUpdateDestroyView.as_view(),
+        name='user_profile'
+        ),
+    url(r'^profile/(?P<pk>\d+)/edit/$',
+        apis.UserRetrieveUpdateDestroyView.as_view(),
+        name='userinfo_update'
+        ),
     url(r'signup/$',
         apis.UserSignupView.as_view(),
         name='signup'
@@ -21,10 +29,6 @@ urlpatterns = [
     url(r'^logout/$',
         apis.UserLogoutView.as_view(),
         name='logout'
-        ),
-    url(r'^(?P<pk>\d+)/edit/$',
-        apis.UserRetrieveUpdateDestroyView.as_view(),
-        name='userinfo_update'
         ),
     url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
         apis.AccountActivationView.as_view(),
