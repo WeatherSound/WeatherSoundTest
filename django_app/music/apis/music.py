22136d318980feebdb51828cad10c13c42233bf2
pignuante <syaoran215@naver.com>
2017-08-17 18:35:42 +0900

    [deploy] first fin

--- django_app/music/apis/music.py
+++ django_app/music/apis/music.py
@@ -22,15 +22,6 @@ __all__ = (
 User = get_user_model()
 
 
-# APIView 사용
-# url 형식 :
-# {
-#   "id": 1,
-#   "img_music": "/media/img_music/logo_icecream.jpg",
-#   "name_music": "응프리스타일",
-#   "name_singer": "sik-k, punchnello, flowsik",
-#   "file_music": "/media/music/EUNG_FREESTYLE_%E1%84%8B%E1%85%B3%E1%86%BC%E1%84%91%E1%85%B3%E1%84%85%E1%85%B5%E1%84%89%E1%85%B3%E1%84%90%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF_-_LIVE_SIK-K_PUNCHNELLO_OWEN_OVADOZ_FLOWSIK.mp3"
-# },
 class MusicListCreateView(APIView):
     def get(self, request, *args, **kwargs):
         musics = Music.objects.all()
@@ -41,15 +32,6 @@ class MusicListCreateView(APIView):
         pass
 
 
-# GenericView 사용
-# url 형식 :
-# {
-#   "id": 1,
-#   "img_music": "http://localhost:8000/media/img_music/logo_icecream.jpg",
-#   "name_music": "응프리스타일",
-#   "name_singer": "sik-k, punchnello, flowsik",
-#   "file_music": "http://localhost:8000/media/music/EUNG_FREESTYLE_%E1%84%8B%E1%85%B3%E1%86%BC%E1%84%91%E1%85%B3%E1%84%85%E1%85%B5%E1%84%89%E1%85%B3%E1%84%90%E1%85%A1%E1%84%8B%E1%85%B5%E1%86%AF_-_LIVE_SIK-K_PUNCHNELLO_OWEN_OVADOZ_FLOWSIK.mp3"
-# },
 class MusicListView(generics.ListCreateAPIView):
     queryset = Music.objects.all().order_by('name_album')
     serializer_class = MusicSerializer
@@ -131,6 +113,7 @@ class UserMusiclistRetrieveUpdateDestroy(generics.RetrieveUpdateAPIView):
         return Response(context, status=status.HTTP_200_OK)
 
     # TODO 리스트 내에 음악 중복처리
+    # 플레이리스트 리스트 + 리스트에 음악 추가
     def put(self, request, *args, **kwargs):  # make peronal list
         try:
             user = User.objects.get(pk=kwargs["pk"])
@@ -198,8 +181,9 @@ class UserPlayListMusicsRetrieveDestroy(generics.RetrieveUpdateDestroyAPIView):
         serializer = self.serializer_class(queryset)
         return Response(serializer.data)
 
+    # Playlist 음악 삭제
     def put(self, request, *args, **kwargs):
-        music_deleted = request.data.get("music_deleted", None)
+        music_deleted = request.data.get("name", None)
         if music_deleted:
             try:
                 pk = kwargs["pk"]
@@ -219,6 +203,11 @@ class UserPlayListMusicsRetrieveDestroy(generics.RetrieveUpdateDestroyAPIView):
                     "detail": "Music does not exsists",
                 }
                 return Response(context, status=status.HTTP_404_NOT_FOUND)
+            except Exception as e:
+                context = {
+                    "deetail": "Enter Integer",
+                }
+                return Response(context, status=status.HTTP_406_NOT_ACCEPTABLE)
             music.delete()
             # TODO 빈 리스트면 삭제
             # Playlist.objects.make_playlist_id()
@@ -261,25 +250,3 @@ class PlaylistListCreateView(APIView):
         # user = User.objects.all()
         serializer = PlaylistSerializer(playlists, many=True)
         return Response(serializer.data)
-
-        # def post(self, request, *args, **kwargs):
-        #     pass
-
-        # 개인이 소유한 플레이 리스트
-        # class UserPlaylistListCreateView(APIView):
-        #     def get(self, request, *args, **kwargs):
-        #         # TODO 유저별로 판단 가능하게!
-        #         users = User.objects.all()
-        #         serializer = UserPlaylistSerializer(users, many=True)
-        #         return Response(serializer.data)
-        #
-        #     def post(self, request, *args, **kwargs):
-        #         pass
-
-        # class PlaylistMusicsListCreateView(APIView):
-        #     def get(self, request, *args, **kwargs):
-        #         plm = PlaylistMusics.objects.all()
-        #         serializer = PlaylistMusicsSerializer(plm, many=True)
-        #         return Response(serializer.data)
-        #
-        #     pass
